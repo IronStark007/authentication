11af9801326751906cfa2d5182f6d954608ceb72
Mohd Ali Ansari <ansar326629@gmail.com>
2023-02-26 18:49:18 +0530

    feat/improved_login_logic
    - Improved login controller logic
    
    - Bumped version

--- src/controller/user.js
+++ src/controller/user.js
@@ -7,18 +7,28 @@ const {
 const { v4: uuidv4 } = require("uuid");
 
 const register = (req, res) => {
-  let { salt, hash } = genHashPassword(req.validateData.password);
-  let newUser = {
-    id: uuidv4(),
-    username: req.validateData.username,
-    salt: salt,
-    hash: hash,
-  };
-  createUser(newUser, (result) => {
-    if (result instanceof Error) {
-      res.status(500).send({ succes: false, error: result.detail });
+  getUser(req.validateData.username, (user) => {
+    if (user instanceof Error) {
+      res.status(500).send({ succes: false, error: user.detail });
+    } else if (!user) {
+      res
+        .status(404)
+        .send({ succes: false, error: "username does not exists" });
     } else {
-      res.send({ success: true, user: newUser });
+      let { salt, hash } = genHashPassword(req.validateData.password);
+      let newUser = {
+        id: uuidv4(),
+        username: req.validateData.username,
+        salt: salt,
+        hash: hash,
+      };
+      createUser(newUser, (result) => {
+        if (result instanceof Error) {
+          res.status(500).send({ succes: false, error: result.detail });
+        } else {
+          res.send({ success: true, user: newUser });
+        }
+      });
     }
   });
 };
